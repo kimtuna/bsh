61b064991bbc1df00233c81a1dccb77bc9fec3a0
kdw240 <kdw240@gmail.com>
2025-06-03 17:32:57 +0900

    chore: 버전 0.1.3으로 업데이트

--- Formula/bsh.rb
+++ Formula/bsh.rb
@@ -1,15 +1,10 @@
 class Bsh < Formula
   desc "Blockchain Server Hosting CLI"
   homepage "https://github.com/kimtuna/bsh"
-  version "0.1.2"
+  version "0.1.3"
+  url "https://github.com/kimtuna/bsh/releases/download/v0.1.3/bsh-darwin-arm64.tar.gz"
+  sha256 "84299584d26e2357b4e3c35c695c44cd182fdcab7a48541a63bd5a488413d167"
   license "MIT"
-
-  on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/kimtuna/bsh/releases/download/v0.1.2/bsh-darwin-arm64.tar.gz"
-      sha256 "566462f87613c01d71018306902e5d063e7dc1f2fb623b78d0d16a0981f4a1f5"
-    end
-  end
 
   def install
     bin.install "bsh"
